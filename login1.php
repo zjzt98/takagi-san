301d0e6f3a05831123040a9e74bb425a6cbe3900
zjzt98 <107230103+zjzt98@users.noreply.github.com>
2022-07-01 11:21:57 +0800

    Add files via upload

---
+++ login1.php
@@ -0,0 +1,2 @@
+欢迎<?php echo $_POST["accounts"]; ?>!<br>
+你的年龄是 <?php echo $_POST["passwords"]; ?>  岁。
---
+++ search.php
@@ -0,0 +1,2 @@
+欢迎<?php echo $_POST["user_search"]; ?>!<br>
+你的年龄是 <?php echo $_POST["passwords"]; ?>  岁。
